77f2b3cbd9581d2c308c521168eb8642750b8c74
Cleciane <cleciane.oliveira.silva@gmail.com>
2026-01-13 00:54:10 -0800

    Completed Week 1: Hello GitHub assignment

--- modules/week-01-hello-github/starter/Program.cs
+++ modules/week-01-hello-github/starter/Program.cs
@@ -4,6 +4,8 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Console.Write("Hello, GitHub!");
+
         // TODO: Write code that outputs "Hello, GitHub!" to the console
     }
 }
